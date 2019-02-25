a9144ec95d1e60641a393444ef4437579442860b
Eugene <orexjeka@icloud.com>
2019-02-25 22:03:46 +0300

    add model name

---
+++ exSwift/Common/Extensions/UIKit/UIDevice+ModelName.swift
@@ -0,0 +1,64 @@
+//
+//  UIDevice+ModelName.swift
+//  exSwift
+//
+//  Created by Eugene on 25/02/2019.
+//  Copyright © 2019 Eugene. All rights reserved.
+//
+
+import UIKit
+
+extension UIDevice {
+    
+    // Models that supports iOS >= 10.0
+    var modelName: String {
+        
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        switch identifier {
+        case "iPod7,1":                                 return "iPod Touch 6"
+        case "iPhone5,1", "iPhone5,2":                  return "iPhone 5"
+        case "iPhone5,3", "iPhone5,4":                  return "iPhone 5c"
+        case "iPhone6,1", "iPhone6,2":                  return "iPhone 5s"
+        case "iPhone7,2":                               return "iPhone 6"
+        case "iPhone7,1":                               return "iPhone 6 Plus"
+        case "iPhone8,1":                               return "iPhone 6s"
+        case "iPhone8,2":                               return "iPhone 6s Plus"
+        case "iPhone9,1", "iPhone9,3":                  return "iPhone 7"
+        case "iPhone9,2", "iPhone9,4":                  return "iPhone 7 Plus"
+        case "iPhone8,4":                               return "iPhone SE"
+        case "iPhone10,1", "iPhone10,4":                return "iPhone 8"
+        case "iPhone10,2", "iPhone10,5":                return "iPhone 8 Plus"
+        case "iPhone10,3", "iPhone10,6":                return "iPhone X"
+        case "iPhone11,2":                              return "iPhone XS"
+        case "iPhone11,4", "iPhone11,6":                return "iPhone XSMax"
+        case "iPhone11,8":                              return "iPhone XR"
+            
+        case "iPad3,4", "iPad3,5", "iPad3,6":           return "iPad 4"
+        case "iPad4,1", "iPad4,2", "iPad4,3":           return "iPad Air"
+        case "iPad5,3", "iPad5,4":                      return "iPad Air 2"
+        case "iPad6,11", "iPad6,12":                    return "iPad 5" // iPad 2017
+        case "iPad7,5", "iPad7,6":                      return "iPad 6" //iPad 2018
+        case "iPad4,4", "iPad4,5", "iPad4,6":           return "iPad Mini 2"
+        case "iPad4,7", "iPad4,8", "iPad4,9":           return "iPad Mini 3"
+        case "iPad5,1", "iPad5,2":                      return "iPad Mini 4"
+        case "iPad6,3", "iPad6,4":                      return "iPad Pro (9.7-inch)"
+        case "iPad6,7", "iPad6,8":                      return "iPad Pro (12.9-inch)"
+        case "iPad7,1", "iPad7,2":                      return "iPad Pro (12.9-inch) (2nd generation)"
+        case "iPad7,3", "iPad7,4":                      return "iPad Pro (10.5-inch)"
+        case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4":return "iPad Pro (11-inch)"
+        case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8":return "iPad Pro (12.9-inch) (3rd generation)"
+            
+        case "i386", "x86_64":                          return "Simulator"
+            
+        default:                                        return identifier
+        }
+    }
+    
+}
